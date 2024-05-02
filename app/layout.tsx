67da589f4b3fcbb10879940fdd6d6a7718d7808f
Parth Awasthi <107567458+NotHumanFF@users.noreply.github.com>
2024-05-02 22:15:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,16 +12,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-
-
-
-// Secrets
-// POSTGRES_URL="postgres://default:keLjysF5Y7po@ep-morning-water-a4d1bpsw-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
-// POSTGRES_PRISMA_URL="postgres://default:keLjysF5Y7po@ep-morning-water-a4d1bpsw-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15"
-// POSTGRES_URL_NO_SSL="postgres://default:keLjysF5Y7po@ep-morning-water-a4d1bpsw-pooler.us-east-1.aws.neon.tech:5432/verceldb"
-// POSTGRES_URL_NON_POOLING="postgres://default:keLjysF5Y7po@ep-morning-water-a4d1bpsw.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
-// POSTGRES_USER="default"
-// POSTGRES_HOST="ep-morning-water-a4d1bpsw-pooler.us-east-1.aws.neon.tech"
-// POSTGRES_PASSWORD="keLjysF5Y7po"
-// POSTGRES_DATABASE="verceldb"
